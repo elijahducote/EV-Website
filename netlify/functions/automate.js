37e4496bcb5e52d490b84f34a6efcde9a3a87be8
elijahducote <validpupflower@gmail.com>
2024-04-08 14:02:54 -0500

    update netlify/functions/automate.js

--- netlify/functions/automate.js
+++ netlify/functions/automate.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 var payload = [],
+poplus = [],
+stats,
 albums,
 sorted,
 json,
+leng,
+i,
+cur,
 sha,
 msgcode,
 fyl,
@@ -49,6 +54,15 @@ catch (error) {
 }
 
 try {
+await axios.get("https://api.spotify.com/v1/artists/5hTbr7Q7inWI4pYOcOPsH0/top-tracks?country=US",{headers:{"Authorization": `Bearer ${token}`}})
+ .then(response => {
+    if (response.status === 200) stats = response.data.tracks;
+    else throw new Error("Uh, oh! " + response.status);
+});
+leng = stats.length;
+for (i = leng;i;--i) {
+cur = leng - i;
+poplus[cur] = {name:stats[cur].name,url:stats[cur].external_urls.spotify,album:stats[cur].album.name};
 await axios.get("https://api.spotify.com/v1/artists/5hTbr7Q7inWI4pYOcOPsH0/albums?include_groups=album&limit=50",{headers:{"Authorization": `Bearer ${token}`}})
  .then(response => {
     if (response.status === 200) albums = response.data.items;
@@ -76,6 +90,7 @@ for (NTH = arrlen;NTH;--NTH) {
   payload.push({name:sorted[arrlen - NTH].name,url:sorted[arrlen - NTH].external_urls.spotify,cover:sorted[arrlen - NTH].images[0].url,date:sorted[arrlen - NTH].release_date,id:sorted[arrlen - NTH].id});
 }
 json.discography = payload;
+json.tracks = poplus;
 fyl = JSON.stringify(json);
 await axios.put("https://api.github.com/repos/elijahducote/Ev-Website/contents/automation.json",{"message":"update file","sha":sha,"content":Buffer.from(fyl).toString("base64")},{headers:{"Accept":"application/vnd.github+json","Authorization":`Bearer ${process.env.TOKEN}`,"X-GitHub-Api-Version":"2022-11-28"}}).then(response => {
     if (response.status === 200) {
